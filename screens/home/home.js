2f697b5ab07150bd81a5e9d43e556e18b8c52493
carlossantosrj1988-creator <carlossantosrj1988@gmail.com>
2026-05-06 05:42:25 -0300

    Update home.js

--- screens/home/home.js
+++ screens/home/home.js
@@ -1,1 +1,10 @@
+const homeScreen = document.createElement('div');
+homeScreen.id = 'home-screen';
 
+homeScreen.innerHTML = `
+  <div id="home-logo">PATF</div>
+  <div id="home-subtitle">PAST AND THE FUTURE TCG</div>
+  <div id="home-start">TOQUE PARA COMEÇAR</div>
+`;
+
+document.body.appendChild(homeScreen);
